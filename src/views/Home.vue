9fd3b6d9ecfb42eeff3079ea7b19d6dfded9a93f
Juan Atehortua <juandiegox17@gmail.com>
2021-10-10 14:43:41 -0400

    Updated logo and getting ready to customize bulma colors

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="home">
-    Welcome to the website
+    Welcome to Discussion Community
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Home',
-}
+  name: "Home",
+};
 </script>
